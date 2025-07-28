b8baddb5667c4e2870a331a2aab947052e4eba7d
Sophie <sunfloweryam@gmail.com>
2025-07-28 09:56:16 -0700

    Initial Commit

---
+++ TO-DoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TO-DoList
+//
+//  Created by Sheng Saechao on 7/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TO-DoList/TO_DoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TO_DoListApp.swift
+//  TO-DoList
+//
+//  Created by Sheng Saechao on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TO_DoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
